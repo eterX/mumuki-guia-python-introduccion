b80c0e762d9d87edad5dc787cc8d3c2e95668720
agus@mumuki.org <agus@mumuki.org>
2017-12-21 14:12:26 -0500

    Mumuki Export on 2017-12-21 14:12:26 -0500

--- 00002_Un saludo cordial/test.py
+++ 00002_Un saludo cordial/test.py
@@ -1,6 +1,8 @@
-class Test(unittest.TestCase):
-  def saludo_cordial(nombre, edad):
+def saludo_cordial(nombre, edad):
     /*...content...*/
+    
+class Test(unittest.TestCase):
+  
 
   def test_hola_carlos(self):
     self.assertEquals(saludo_cordial("carlos", 98), 'Hola Carlos, 98 anos no es nada, estas hecho un pibe!')
