5c2a0c26011ad48e959b0e4b59ce3c2e055039d9
fedescarpa@gmail.com <fedescarpa@gmail.com>
2018-09-06 09:06:33 -0400

    Mumuki Export on 2018-09-06 09:06:33 -0400

--- 00001_Hola Mundo/test.py
+++ 00001_Hola Mundo/test.py
@@ -2,6 +2,4 @@ class Test(unittest.TestCase):
 
   def test_hola_mumukito(self):
     self.assertEquals(hola_mumukito(), 'Hola Mumukito!')
-  
-  def test_hola_mumukito(self):
-    self.assertTrue(True)
+
